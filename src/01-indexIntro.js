f83de57556f51c01c7a307c8221b17fc3c2a1fc7
Luciano De Pasquale <luchodepasquale@gmail.com>
2024-08-03 19:21:57 +0200

    first commit

---
+++ src/01-indexIntro.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const store = configureStore({
+  reducer: {
+      state: (state = 0, action) => {
+          switch (action.type) {
+              case 'incrementar':
+                  return state + 1
+              case 'decrementar':
+                  return state - 1
+              case 'set':
+                  return action.payload
+              default: return state
+              }
+      },
+   },
+})
+console.log(store.getState())
+store.dispatch({ type: 'lala' })
+console.log(store.getState())
+store.dispatch({ type: 'incrementar' })
+console.log(store.getState())
+store.dispatch({ type: 'decrementar' })
+
+console.log(store.getState())
+
+store.dispatch({ type: 'set', payload: 15 })
+console.log(store.getState())
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useDispatch, useSelector } from "react-redux"
+import { useState } from "react"
+const initialState = {
+    entities: [],
+}
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'todo/add': {
+        return {
+            ...state, 
+            entities: state.entities.concat({ ...action.payload })
+        }
+      } 
+      default:
+        return state
+    }
+   
+}
+const TodoItem = ({ todo }) => {
+    return (
+        <li>{todo.title}</li>
+    )
 }
 
-export default App;
+const App = () => {
+    const [value, setValue] = useState('')
+    const dispatch = useDispatch()
+    const state = useSelector(x => x)
+    const submit = e => {
+        e.preventDefault()
+        if (!value.trim()) {
+            return
+        }
+    }
+
+    const id = Math.random().toString(36)
+    const todo = { title: value, completed: false, id }
+    dispatch({ type: 'todo/add', payload: todo })
+    setValue('')
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <input value={value} onChange={e => setValue(e.target.value)} />
+            </form>
+            <button>Mostrar ToDos</button>
+            <button>Completados</button>
+            <button>Incompletos</button>
+            <ul>
+                {state.entities.map(todo => <TodoItem key={todo.id} todo={todo} />)}
+            </ul>
+        </div>
+    )
+}
+
+export default App
